aa1a2762c82510b06c116839df7850eeb06fbfb0
Fahim Fiyaz <faiyazfahim743@gmail.com>
2025-02-15 16:03:45 +0600

    Onbording screen updated

--- SkillTap/test/widget_test.dart
+++ SkillTap/test/widget_test.dart
@@ -7,7 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:skillset/app.dart';
+import 'package:skilltap/app.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
